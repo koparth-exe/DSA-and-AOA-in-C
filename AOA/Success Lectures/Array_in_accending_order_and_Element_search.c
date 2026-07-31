2b5c96df2e01b4b726a870c1e62f97fe522e1066
koparth.exe <parth.korgaonkar07@gmail.com>
2026-07-31 20:27:31 +0530

    Initial commit of DSA and AOA concepts

---
+++ AOA/Success Lectures/Array_in_accending_order_and_Element_search.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int a[6];
+    int i, num;
+    for (i = 0; i <=5; i++)
+    {
+        printf("Enter and fill number at a[%d]: ", i);
+        scanf("%d", &a[i]);
+    }
+    
+    printf("Enter a number to find it in array: ");
+    scanf("%d", &num);
+    for(i = 0; i <= 5; i++)
+    {
+        if (a[i] == num)
+        {
+            printf("Element found!");
+            break;
+        }
+        else if (i = 6)
+        {
+            printf("Element not found!");
+        }
+    }
+    return 0;
+}
+
+
